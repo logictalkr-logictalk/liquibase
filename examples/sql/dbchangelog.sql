9cf4da253d900ed2ad0d26c2f4d929bd43623b83
janakiramaiah186 <janakiramaiah186@gmail.com>
2021-04-08 19:57:51 +0530

    'load'

---
+++ examples/sql/dbchangelog.sql
@@ -0,0 +1,11 @@
+--CREATE TABLE Persons (
+  --  PersonID int,
+   -- LastName varchar(255),
+   -- FirstName varchar(255),
+    --Address varchar(255),
+    --City varchar(255));
+
+--changeset nvoxland:2  
+insert  into  Persons  (PersonID,  LastName, FirstName, Address, City)  values  (1, 'nemali','janu','nehru','macherla');  
+
+
