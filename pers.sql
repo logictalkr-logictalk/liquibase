baaf66a2a37541792c798237de0d649d67c8a4f2
janakiramaiah186 <janakiramaiah186@gmail.com>
2021-04-15 19:45:58 +0530

    'add'

---
+++ pers.sql
@@ -0,0 +1,2 @@
+insert  into  Persons  (PersonID,  LastName, FirstName, Address, City)  values  (9, 'lali','ik','nehru','thalapalli');
+
--- person3.sql
+++
@@ -1,2 +0,0 @@
-insert  into  Persons  (PersonID,  LastName, FirstName, Address, City)  values  (9, 'lali','ik','nehru','thalapalli');
-insert  into  Persons  (PersonID,  LastName, FirstName, Address, City)  values  (10, 'mouni','shaik','nehru','thalapalli');
