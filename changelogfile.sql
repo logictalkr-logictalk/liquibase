c49473213e22fe66396c746a5651a763e959d79c
janakiramaiah186 <janakiramaiah186@gmail.com>
2021-04-16 15:44:34 +0530

    'add'

--- changelogfile.sql
+++ changelogfile.sql
@@ -1,4 +1,3 @@
 --liquibase formatted sql
 --changeset {authorName}:{id}
 insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (205, 'siva','s','macherla','macherla');
-<include file = 'dbchangelog.sql'>
--- dbchangelog.sql
+++ dbchangelog.sql
@@ -1,7 +1,12 @@
 --liquibase formatted sql
 --changeset {authorName}:{id}
-CREATE TABLE Emp (PersonID int,
+CREATE TABLE company (PersonID int,
     LastName varchar(255),
 	FirstName varchar(255),
 	Address varchar(255),
 	City varchar(255));
+	
+	
+insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (200, 'venkat','p','kothapalli','macherla');
+insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (201, 'janakiramaiah','N','nehru nagar','macherla');
+insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (202, 'moulali','sk','thalapalli','macherla');
