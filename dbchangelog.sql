5eda6a71626b9647125181e8c422958c211839e1
janakiramaiah186 <janakiramaiah186@gmail.com>
2021-04-16 08:50:20 +0530

    'add'

--- dbchangelog.sql
+++ dbchangelog.sql
@@ -1,9 +1,7 @@
+--liquibase formatted sql
+--changeset {authorName}:{id}
 CREATE TABLE Emp (PersonID int,
     LastName varchar(255),
 	FirstName varchar(255),
 	Address varchar(255),
 	City varchar(255));
-
- 
-
-
