846aaf493734ac0fba19fe7cde727b07ae82df44
janakiramaiah186 <janakiramaiah186@gmail.com>
2021-04-18 10:07:55 +0530

    'comitee'

--- changelogfiles/changelogfile.sql
+++ changelogfiles/changelogfile.sql
@@ -1,4 +1,3 @@
 --liquibase formatted sql
 --changeset {authorName}:{id}
-insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (205, 'siva','s','macherla','macherla');
 insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (206, 'sivakumar','s','kote','macherla');
