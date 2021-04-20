62d19c60b98ff8c3e979a1d9bf12a4c33b07e2e7
janakiramaiah186 <janakiramaiah186@gmail.com>
2021-04-20 11:28:35 +0530

    delete pf

--- changelogfiles/changelogfile.sql
+++ changelogfiles/changelogfile.sql
@@ -1,3 +1,4 @@
 --liquibase formatted sql
 --changeset {authorName}:{id}
-insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (206, 'sivakumar','s','kote','macherla');
+insert into Employee (PersonID,  LastName, FirstName, Address, City)  values  (205, 'siva','s','macherla','macherla');
+
